609ac675e4c1a45f537a1386469917d420e44f9c
ms3 <ms3.msssivadas@gmail.com>
2025-09-16 14:21:18 +0530

    update optimization

--- haloutils/optimize_hod.py
+++ haloutils/optimize_hod.py
@@ -2,7 +2,7 @@
 # the observed values. 
 
 
-import logging, numpy as np
+import logging, numpy as np, numpy.typing as nt
 from scipy.special import erf
 from scipy.integrate import simpson
 from scipy.optimize import minimize
@@ -10,13 +10,13 @@ from scipy.optimize import minimize
 def optimizer1(
         n_gal       : float,
         f_sat       : float,
-        mass_func   : list[tuple[float, float]], 
+        mass_func   : nt.NDArray[np.float64], 
         sigma_m     : float                 = 0.,
         alpha       : float                 = 1.,
         mmin_bounds : tuple[float, float]   = (1e+12, 1e+18),
         m1_bounds   : tuple[float, float]   = (1e+13, 1e+18),
         tol         : float                 = 1e-08,
-        gridsize    : int | tuple[int, int] = (11, 11)
+        gridsize    : int | tuple[int, int] = (11, 11),
     ) -> tuple[float, float, float, float, float]:
 
     logger = logging.getLogger()
@@ -28,36 +28,36 @@ def optimizer1(
     # Getting the log of mass in Msun and dn/dln(m) (Mpc^-3) from the table:
     lnm, dndlnm = np.log(mass_func[:, 0]), mass_func[:, 1]
 
-    def cost(p: tuple[float, float], return_parts = False) -> float:
-
+    def get_observables(p: tuple[float, float]) -> tuple[float, float]:
         lnm_min, lnm1 = p
-        lnm0 = lnm_min 
+        lnm0 = lnm_min    # setting M0 = M_min
 
         # Central galaxy count function
-        arg = lnm - lnm_min
-        if sigma_m < 1e-08: 
-            central_count = np.heaviside(arg, 1.)
-        else:
-            central_count = 0.5*( 1. + erf(arg / sigma_m) )
+        arg       = lnm - lnm_min
+        n_central = ( 
+            0.5*(1. + erf(arg / sigma_m)) if sigma_m > 0. # sigmoid function
+            else np.heaviside(arg, 1.)                    # step function, if sigma=0
+        )
         
         # Satellite galaxy count function
-        arg = np.exp(lnm - lnm1) - np.exp(lnm0 - lnm1)
-        satellite_count = central_count * np.maximum(arg, 0.)**alpha
+        arg         = np.exp(lnm - lnm1) - np.exp(lnm0 - lnm1)
+        n_satellite = n_central * np.maximum(arg, 0.)**alpha
 
         # Total galaxy density 
-        ngal_test = simpson( dndlnm * (central_count + satellite_count), lnm ) # Mpc^3
-        # return ngal_test / n_gal
+        ngal_calc = simpson( dndlnm * (n_central + n_satellite), lnm ) # Mpc^3
         
         # Satellite fraction
-        fsat_test = simpson( dndlnm * satellite_count, lnm ) / ngal_test
-        # return fsat_test / f_sat
+        fsat_calc = simpson( dndlnm * n_satellite, lnm ) / ngal_calc
 
+        return ngal_calc, fsat_calc
+
+    def cost(p: tuple[float, float], return_parts = False) -> float:
         # Cost is the weighted distance of the calculated values from the observed
         # values, with the observed values used as weights. 
+        ngal_test, fsat_test = get_observables(p)
         cost = (ngal_test / n_gal - 1.)**2 + (fsat_test / f_sat - 1.)**2
         cost = np.log( 1e-08 + cost )
-
-        return cost if not return_parts else ( cost, (ngal_test, fsat_test) )
+        return cost
     
     lnm_min_bounds = np.clip( np.log(mmin_bounds), min(lnm), max(lnm) )
     lnm1_bounds    = np.clip( np.log(m1_bounds)  , min(lnm), max(lnm) )
@@ -66,14 +66,26 @@ def optimizer1(
 
     # Guessing initial value using a grid:
     if isinstance(gridsize, int): gridsize = (gridsize, )*2
-    lnm_min_grid = np.linspace(*lnm_min_bounds, gridsize[0])
-    lnm1_grid    = np.linspace(*lnm1_bounds   , gridsize[1])
-    guess_index  = np.unravel_index( 
-        np.argmin([[ cost([a, b]) for b in lnm1_grid ] for a in lnm_min_grid ]), 
-        lnm_min_grid.shape+lnm1_grid.shape, 
+
+    def gridp(i: int, j: int) -> tuple[float, float]: 
+        # Interpolate the values of log(M_min) and log(M1) on the grid.
+        return (
+            np.interp( i, [0, gridsize[0]-1], lnm_min_bounds ), 
+            np.interp( j, [0, gridsize[1]-1], lnm1_bounds    ), 
+        )
+    
+    initial_guess = gridp(
+        *np.unravel_index( 
+            np.argmin(
+                np.fromfunction( 
+                    np.vectorize(lambda i, j: cost( gridp(i, j) ), otypes=[float]), 
+                    gridsize,
+                )
+            ),
+            gridsize, 
+        )
     )
-    initial_guess = ( lnm_min_grid[guess_index[0]], lnm1_grid[guess_index[1]] )
-    logger.info(f"using initial guess: {tuple(initial_guess)}")
+    logger.info(f"using initial guess: {initial_guess}")
 
     # Optimization:
     optim_result = minimize(
@@ -84,24 +96,27 @@ def optimizer1(
     )
     if optim_result.success:
         logger.info(f"optimization successful, message: {optim_result.message!r}")
-        logger.info(f"reached optimum value: {tuple(optim_result.x)} in {optim_result.nit} iterations")
+        logger.info(f"reached optimum value: {optim_result.x} in {optim_result.nit} iterations")
     else:
         logger.info(f"optimization failed, message: {optim_result.message!r}")
+    lnm_min, lnm1 = float(optim_result.x[0]), float(optim_result.x[1])
 
-    ngal_opt, fsat_opt = cost(optim_result.x, return_parts = True)[1]
+    ngal_opt, fsat_opt = get_observables(optim_result.x)
     logger.info(f"calculated values: n_gal={ngal_opt:.6g}, f_sat={fsat_opt:.6g}")
     
-    optimum_hod = ( optim_result.x[0], sigma_m, optim_result.x[0], optim_result.x[1], alpha )
+    # Optimum HOD parameters: 
+    #               log(M_min) sigma_M  log(M0)  log(M1) alpha
+    optimum_hod = ( lnm_min,   sigma_m, lnm_min, lnm1,   alpha )
     return optimum_hod
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    mass_func = np.exp( np.loadtxt('x.dat') )
-    p = optimizer1(
-        2.5e-05, 
-        0.05, 
-        mass_func, 
-        0., 
-        1., 
-    )
-    print(p)
+# if __name__ == "__main__":
+#     logging.basicConfig(level=logging.INFO)
+#     mass_func = np.exp( np.loadtxt('_data/huge.hmf.dat') )
+#     p = optimizer1(
+#         2.5e-05, 
+#         0.05, 
+#         mass_func, 
+#         0., 
+#         1., 
+#     )
+#     print(p)
