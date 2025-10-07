4557b044046d9c540d2199b84e87135e6d902d4b
ms3 <ms3.msssivadas@gmail.com>
2025-10-08 02:23:04 +0530

    update

--- task.sh
+++ task.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-SIM_NAME="AbacusSummit_hugebase_c000_ph000"
+TASK_ID=12345 # $RANDOM
+
+SIM_NAME="AbacusSummit_hugebase_c000_ph000" 
 REDSHIFT=3
 CATALOG_DIR="_data/abacus"
 NPROCS=-1
 RMIN=0.1
-RMAX=50.0
-NUM_RBINS=21
+RMAX=100.0
+NUM_RBINS=50
+NUM_MBINS=20
 JK_SUBDIVS=4
+VENV=".venv"
 
-TASK_ID=12345 # $RANDOM
-
-source .venv/bin/activate
+source $VENV/bin/activate
 
 # Calculating halo mass-function:
 MASSFUNC_FILE="hmf.$SIM_NAME.z$(printf %04d $((100*REDSHIFT))).txt"
@@ -30,122 +32,112 @@ EOF
     --nprocs   $NPROCS
 fi 
 
+# Calculating mass bins: these mass bins are calculated in the range where 98%
+# halos are present - this makes sure that the larger mass region with lesser 
+# halos are avoided in correlation calculation... 
+read -r -a M_EDGES <<< $(python << EOF
+import numpy as np
+from haloutils.misc.adaptive_binning import adaptive_bins
+bins   = $NUM_MBINS
+x, y   = np.loadtxt("$MASSFUNC_FILE", usecols=[0, 1], unpack=True)
+cdf    = np.hstack([0., np.cumsum( np.diff(x) * (( y[1:] + y[:-1] ) / 2. - min(y)) )])
+k      = cdf < 0.983*cdf[-1]
+medges = np.exp( adaptive_bins(x[k], y[k], (max(x) - min(x)) / bins) )
+print(' '.join([ f"{t:8.4e}" for t in medges ]))
+EOF
+) 
+NUM_MBINS=$((${#M_EDGES[@]}-1))
+
 # Distance bin edges:
 export RBINS=$(python << EOF
-import numpy as np
-print( " ".join([ f"{x:8.4e}" for x in np.logspace(np.log10($RMIN), np.log10($RMAX), $NUM_RBINS) ]) )
+import numpy as np 
+redges = np.logspace( np.log10($RMIN), np.log10($RMAX), $NUM_RBINS + 1 )
+print(' '.join([ f"{x:8.4e}" for x in redges ]))
 EOF
 )
+
+# Combined correlation data:
+CORRFUNC_FILE="xcf.$SIM_NAME.z$(printf %04d $((100*REDSHIFT))).asdf"
+python << EOF
+import numpy as np, asdf
+redges   = np.fromstring("$RBINS", dtype="f8", sep=' ')
+rcenters = np.sqrt( redges[1:]*redges[:-1] ) 
+medges   = np.fromstring("${M_EDGES[@]}", dtype="f8", sep=' ')
+mcenters = np.sqrt( medges[1:]*medges[:-1] ) 
+xi_shape = [$NUM_MBINS, $NUM_MBINS, $NUM_RBINS]
+with asdf.AsdfFile({
+    "header": {}, 
+    "data": {
+        "powerSpectrum": [],
+        "rcenters"     : rcenters, 
+        "mcenters"     : mcenters,
+        "xi"           : np.zeros(xi_shape, dtype="f8"), 
+        "xiError"      : np.zeros(xi_shape, dtype="f8"),     
+        "haloMassfunc" : np.loadtxt("$MASSFUNC_FILE", usecols=[0, 1])            
+    }
+}) as af:
+    af.write_to( "$CORRFUNC_FILE", all_array_compression = 'zlib' )
+EOF
 
 CORR_WDIR="corr_$TASK_ID"
 if [[ ! -d "$CORR_WDIR" ]]; then
     mkdir -p $CORR_WDIR
 fi
 
-# Calculating mass bins:
-MBINS_COUNT=5
-read -r -a M_EDGES <<< $(python << EOF
-import numpy as np
-from numpy import loadtxt, hstack, cumsum, diff, exp
-from haloutils.misc.adaptive_binning import adaptive_bins
-bins = $MBINS_COUNT
-x, y = loadtxt("$MASSFUNC_FILE", unpack=True)
-cdf  = hstack([0., cumsum( diff(x) * (( y[1:] + y[:-1] ) / 2. - min(y)) )])
-m    = cdf < 0.983*cdf[-1]
-xb   = exp( adaptive_bins(x[m], y[m], (max(x) - min(x)) / bins) )
-print(' '.join([f"{t:8.4e}" for t in xb]))
-EOF
-) 
-MBINS_COUNT=$((${#M_EDGES[@]}-1))
-
 # Correlation function calculations:
-CORR_FILES="["
-for ((I=0; I<$MBINS_COUNT; I++)); do
-    MRANGE_I="${M_EDGES[I]} ${M_EDGES[I+1]}"
-    for ((J=$I; J<$MBINS_COUNT; J++)); do
-        MRANGE_J="${M_EDGES[J]} ${M_EDGES[J+1]}"
-        CORRFUNC_FILE="$CORR_WDIR/$TASK_ID.$I.$J.asdf"
+CORR_FILES=""
+for ((I=0; I<$NUM_MBINS; I++)); do
+    MA_I="${M_EDGES[I]}"
+    MB_I="${M_EDGES[I+1]}"
+    for ((J=$I; J<$NUM_MBINS; J++)); do
+        MA_J="${M_EDGES[J]}"
+        MB_J="${M_EDGES[J+1]}"
+        CORR_FILE_IJ="$CORR_WDIR/$I.$J.asdf"
         python haloutils/correlation2.py abacus-corrfunc \
         --simname  $SIM_NAME      \
         --redshift $REDSHIFT      \
-        --mrange1  $MRANGE_I      \
-        --mrange2  $MRANGE_J      \
-        --outfile  $CORRFUNC_FILE \
+        --mrange1  $MA_I $MB_I    \
+        --mrange2  $MA_J $MB_J    \
+        --outfile  $CORR_FILE_IJ  \
         --workdir  $CORR_WDIR     \
         --loc      $CATALOG_DIR   \
         --nthreads $NPROCS        \
         --subdivs  $JK_SUBDIVS
-        if [[ -e "$CORRFUNC_FILE" ]]; then
-            CORR_FILES+="(($I,$J),(${MRANGE_I// /,}),(${MRANGE_J// /,}),\"$CORRFUNC_FILE\"),"
+        if [[ -e "$CORR_FILE_IJ" ]]; then
+            # Calculating correlation function from counts and update final output:
+            python << EOF
+import numpy as np, asdf
+from haloutils.correlation2 import correlation_from_count, pcresult
+with asdf.open("$CORRFUNC_FILE") as afo, asdf.open("$CORR_FILE_IJ") as afi:
+    afo["header"] = { 
+        k: v 
+            for k, v in afi["header"].items() 
+            if k not in [ "jackknifeSamples", "mrange1", "mrange2" ]
+    }
+    xi      = correlation_from_count(pcresult( **afi["data"]["pairCounts_full"] ), "ls")
+    xi_jack = correlation_from_count(pcresult( **afi["data"]["pairCounts_jack"] ), "ls")
+    error   = np.sqrt( 
+        ( len(xi_jack)-1 ) * np.mean(( xi_jack - np.mean(xi_jack, axis=0) )**2, axis=0) 
+    )
+    afo["data"]["xi"][i,j,:], afo["data"]["xiError"][i,j,:] = xi, error
+    if i != j:
+        afo["data"]["xi"][j,i,:], afo["data"]["xiError"][j,i,:] = xi, error
+    afo["data"]["powerSpectrum"] = afi["data"]["powerSpectrum"]
+    afo.write_to( "$CORRFUNC_FILE", all_array_compression = 'zlib' )
+EOF
+            CORR_FILES+="$CORR_FILE_IJ "
         fi
     done
 done
-CORR_FILES+="]"
 
-# Combining correlation functions:
-CORRFUNC_FILE="xcf.$SIM_NAME.z$(printf %04d $((100*REDSHIFT))).asdf"
-python << EOF
-import numpy as np, asdf
-from haloutils.correlation2 import correlation_from_count, pcresult
-
-def geom_mean(x, y): 
-    return np.sqrt(x*y)
-
-def calc_error(samples):
-    n    = len(samples)
-    mean = np.mean( samples, axis=0 )
-    return np.sqrt( ( n-1 ) * np.mean( (samples - mean)**2, axis=0 ) )
-
-def combine_corr_data(files, outfile):
-    if not files: return
-    
-    with asdf.open(files[0]) as af: 
-        n_rbins = af["data"]["rbins"].shape[0]
-        header = af["header"]
-        for attr in [ "jackknifeSamples", "mrange1", "mrange2" ]:
-            header.pop( attr, None ) 
-        pktab    = np.array( af["data"]["powerSpectrum"] )
-        rbins    = np.array( af["data"]["rbins"] )
-        rcenters = np.sqrt( rbins[1:]*rbins[:-1] )
-    im, jm    = max(ij for ij, *_ in files )
-    value     = np.zeros((im+1, jm+1, rcenters.shape[0])) 
-    error     = np.zeros_like(value)
-    mcenters1 = np.zeros((im+1,))
-    mcenters2 = np.zeros((jm+1,))
-    for (i, j), file in files:
-        with asdf.open(file) as af:
-            header, data = af["header"], af["data"]
-            mcenters1[i] = geom_mean( *header["mrange1"] )
-            mcenters2[j] = geom_mean( *header["mrange2"] )
-            value[i,j,:] = correlation_from_count(pcresult( **data["pairCounts_full"] ), "ls")
-            error[i,j,:] = calc_error( correlation_from_count(pcresult( **data["pairCounts_jack"] ), "ls"))
-            value[i,j,:] = value[j,i,:]
-            error[i,j,:] = error[j,i,:]
-   
-    with asdf.AsdfFile({
-        "header": header, 
-        "data": {
-            "powerSpectrum": pktab,
-            "rcenters"     : rcenters, 
-            "mcenters1"    : mcenters1,               
-            "mcenters2"    : mcenters2,
-            "xi"           : value, 
-            "xiError"      : error,                 
-        }
-    }) as af:
-        af.write_to( 
-            outfile, 
-            all_array_compression = 'zlib', 
-        )
-    return
-
-combine_corr_data(
-    files   = $CORR_FILES, 
-    outfile = "$CORRFUNC_FILE", 
-)
-EOF
+# Create a zip file of pair counts:
+CORRFUNC_ZIP="xcf.$SIM_NAME.z$(printf %04d $((100*REDSHIFT))).zip"
+if [[ -n "$CORR_FILES" ]]; then
+    zip $CORRFUNC_ZIP $CORR_FILES
+fi 
 
 # Clean-up: 
-# rm -rf "$CORR_WDIR"
+rm -rvf "$CORR_WDIR/*.bin"
+# rm -rvf "$CORR_WDIR"
 
 deactivate
